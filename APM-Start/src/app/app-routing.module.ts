4c09e2cdf26ea809827c9a5f3b947fb571f3fc49
Mohammed Dewidar <dewiidar@yahoo.com>
2019-07-06 17:09:47 +0200

    Lazy loading

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import {PreloadAllModules, RouterModule} from '@angular/router';
 import {WelcomeComponent} from './home/welcome.component';
 import {PageNotFoundComponent} from './page-not-found.component';
+import {AuthGuard} from "./user/auth.guard";
+import {SelectiveStrategyService} from './selective-strategy.service';
 
 
 const routes = [
     {path: 'welcome', component: WelcomeComponent},
     // specifying a default page
+    {
+        path: 'products',
+        canActivate: [AuthGuard],
+        // canLoad: [AuthGuard],
+        data: {preload: true},
+        loadChildren: './products/product.module#ProductModule'
+    },
     {path: '', redirectTo: 'welcome', pathMatch: 'full'},
     // wildcard path, the router matches this path if the url segments don't match any prior path defined in the configurations
     {path: '**', component: PageNotFoundComponent}
@@ -14,11 +23,12 @@ const routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes, {enableTracing: true})
+        RouterModule.forRoot(routes, {preloadingStrategy: SelectiveStrategyService})
     ],
     exports: [
         RouterModule
-    ]
+    ],
+    providers: [SelectiveStrategyService]
 })
 
 export class AppRoutingModule {
--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -10,32 +10,25 @@ import {ProductService} from './product.service';
 import {ProductResolverService} from './product-resolver.service';
 import {ProductEditInfoComponent} from './product-edit/product-edit-info.component';
 import {ProductEditTagsComponent} from './product-edit/product-edit-tags.component';
-import {AuthGuard} from '../user/auth.guard';
 
 const routes = [
     {
-        path: 'products',
-        canActivate: [AuthGuard],
+        path: '',
+        component: ProductListComponent,
+    },
+    {
+        path: ':id',
+        component: ProductDetailComponent,
+        resolve: {product: ProductResolverService}
+    },
+    {
+        path: ':id/edit',
+        component: ProductEditComponent,
+        resolve: {product: ProductResolverService},
         children: [
-            {
-                path: '',
-                component: ProductListComponent,
-            },
-            {
-                path: ':id',
-                component: ProductDetailComponent,
-                resolve: {product: ProductResolverService}
-            },
-            {
-                path: ':id/edit',
-                component: ProductEditComponent,
-                resolve: {product: ProductResolverService},
-                children: [
-                    {path: '', redirectTo: 'info', pathMatch: 'full'},
-                    {path: 'info', component: ProductEditInfoComponent},
-                    {path: 'tags', component: ProductEditTagsComponent}
-                ]
-            }
+            {path: '', redirectTo: 'info', pathMatch: 'full'},
+            {path: 'info', component: ProductEditInfoComponent},
+            {path: 'tags', component: ProductEditTagsComponent}
         ]
     }
 ];
